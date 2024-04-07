7f2a5668bc7d3c04755148ac467fe45e8fa0ecce
voidFunctionReturn0 <voidFunctionReturn0@gmail.com>
2024-04-07 14:38:54 +0900

    p.180

--- test/rumbl_web/controllers/video_controller_test.exs
+++ test/rumbl_web/controllers/video_controller_test.exs
@@ -16,6 +16,28 @@ defmodule RumblWeb.VideoControllerTest do
     end)
   end
 
+  describe "with a logged-in user" do
+    setup %{conn: conn, login_as: username} do
+      user = user_fixture(username: username)
+      conn = assign(conn, :current_user, user)
+
+      {:ok, conn: conn, user: user}
+    end
+
+    @tag login_as: "max"
+    test "lists all user's video on index", %{conn: conn, user: user} do
+      user_video = video_fixture(user, title: "funny cats")
+      other_video = video_fixture(
+        user_fixture(username: "other"),
+        title: "another video")
+
+      conn = get conn, ~p"/manage/videos"
+      assert html_response(conn, 200) =~ ~r/Listing Videos/
+      assert String.contains?(conn.resp_body, user_video.title)
+      refute String.contains?(conn.resp_body, other_video.title)
+    end
+  end
+
   # @create_attrs %{description: "some description", title: "some title", url: "some url"}
   # @update_attrs %{description: "some updated description", title: "some updated title", url: "some updated url"}
   # @invalid_attrs %{description: nil, title: nil, url: nil}
