77b702ebedf6c5b31898ef3518b00393edfae47e
voidFunctionReturn0 <voidFunctionReturn0@gmail.com>
2024-04-07 11:47:48 +0900

    p.169

---
+++ test/rumbl/account_test.exs
@@ -0,0 +1,73 @@
+defmodule Rumbl.AccountTest do
+  use Rumbl.DataCase, async: true
+
+  alias Rumbl.Accounts
+  alias Rumbl.Accounts.User
+
+  describe "register_user/1" do
+    @valid_attrs %{
+      name: "user",
+      username: "eva",
+      password: "secret"
+    }
+
+    @invalid_attrs %{}
+
+    test "with vali data inserts user" do
+      assert {:ok, %User{id: id} = user} = Accounts.register_user(@valid_attrs)
+      assert user.name == @valid_attrs.name
+      assert user.username == @valid_attrs.username
+      assert [%User{id: ^id}] = Accounts.list_users()
+    end
+
+    test "with invalid data does not insert user" do
+      assert {:error, _changeset} = Accounts.register_user(@invalid_attrs)
+      assert Accounts.list_users() == []
+    end
+
+    test "enforces unique usernames" do
+      assert {:ok, %User{id: id}} = Accounts.register_user(@valid_attrs)
+      assert {:error, changeset} = Accounts.register_user(@valid_attrs)
+
+      assert %{username: ["has already been taken"]} = errors_on(changeset)
+      assert [%User{id: ^id}] = Accounts.list_users()
+    end
+
+    test "does not accept long username" do
+      attrs = Map.put(@valid_attrs, :username, String.duplicate("a", 30))
+      {:error, changeset} = Accounts.register_user(attrs)
+
+      assert %{username: ["should be at most 20 character(s)"]} = errors_on(changeset)
+      assert Accounts.list_users() == []
+    end
+
+    test "requires password to be at least 6 chars long" do
+      attrs = Map.put(@valid_attrs, :password, "12345")
+      {:error, changeset} = Accounts.register_user(attrs)
+
+      assert %{password: ["should be at least 6 character(s)"]} = errors_on(changeset)
+      assert Accounts.list_users() == []
+    end
+  end
+
+  describe "authenticate_by_username_and_pass/2" do
+    @pass "123456"
+
+    setup do
+      {:ok, user: user_fixture(password: @pass)}
+    end
+
+    test "returns user with correct password", %{user: user} do
+      assert {:ok, auth_user} = Accounts.authenticate_by_username_and_pass(user.username, @pass)
+      assert auth_user.id == user.id
+    end
+
+    test "returns unauthorized error with invalid password", %{user: user} do
+      assert {:error, :unautorized} = Accounts.authenticate_by_username_and_pass(user.username, "badpass")
+    end
+
+    test "returns not foun error with no matching user for email" do
+      assert {:error, :not_found} = Accounts.authenticate_by_username_and_pass("unknownuser", @pass)
+    end
+  end
+end
