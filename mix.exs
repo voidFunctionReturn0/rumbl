1bf46be134b27c86df7c3d3a99445deb08523e26
voidFunctionReturn0 <voidFunctionReturn0@gmail.com>
2024-04-14 23:26:29 +0900

    p.259

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule Rumbl.MixProject do
   def application do
     [
       mod: {Rumbl.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :observer, :wx]
     ]
   end
 
